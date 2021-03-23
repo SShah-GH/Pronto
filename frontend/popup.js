114b1993fe4a7722b30c5d005fbe0666f091b589
PrestonRooker <74211086+PrestonRooker@users.noreply.github.com>
2021-03-22 21:40:37 -0700

    Settings Theory Testing

--- frontend/popup.js
+++ frontend/popup.js
@@ -5,3 +5,38 @@ document.querySelector('#go-to-options').addEventListener("click", function() {
       window.open(chrome.runtime.getURL('options.html'));
     }
   });
+
+  function setColor() {
+    var favColor;
+    var doesLikeColor;
+    chrome.storage.sync.get([
+      'favoriteColor','likesColor'
+    ],
+     function(items){
+      favColor = items.favoriteColor;
+      doesLikeColor = items.likesColor;
+
+      if(doesLikeColor){
+        if(favColor == 'red'){
+          document.getElementById('circle').style.backgroundColor = 'red';
+        }
+        else if(favColor == 'blue'){
+          document.getElementById('circle').style.backgroundColor = 'blue';
+        }
+        else if(favColor == 'green'){
+          document.getElementById('circle').style.backgroundColor = 'green';
+        }
+        else if(favColor == 'yellow'){
+          document.getElementById('circle').style.backgroundColor = 'yellow';
+        }  
+      }
+      else{
+        document.getElementById('circle').style.backgroundColor = 'transparent';
+      }
+      
+    });
+
+    
+  }
+
+  document.addEventListener('DOMContentLoaded',setColor);
