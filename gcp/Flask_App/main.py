8b53868f645582b807f3d031108944f676f3448d
Satva <satvabshah@gmail.com>
2021-03-26 21:46:40 -0700

    Fixed 403 errors and added rotating API keys

--- gcp/Flask_App/main.py
+++ gcp/Flask_App/main.py
@@ -17,12 +17,15 @@ def RetreiveNews(cat):
     # BBC news api
     # following query parameters are used
     # source, sortBy and apiKey
+    apiKeysList = ["d7fa03ba28f94eba97ffb5276cca060a", "716c0526d1fc42008fdfd127fc0d6f9e", "a9e142b8b32a460497c02018511fbb65", "3670b6fd717a4ad0a49ba042649edef7", "122987b7c1bc4221897f5e361735d413", "196537e4a5814693b334cac3e4723a38", "39810cc78e384d3a9c416070fdeddc64"]
+    apiKey = random.choice(apiKeysList)
+
     query_params = {
       "sortBy": "top",
-      "apiKey": "1c1a415df0f04b0199236dec9d3baf56",
+      "apiKey": apiKey,
       "category"  : cat
     }
-    main_url = " https://newsapi.org/v2/top-headlines?country=us&category=" + cat + "&apiKey=1c1a415df0f04b0199236dec9d3baf56"
+    main_url = " https://newsapi.org/v2/top-headlines?country=us&category=" + cat + "&apiKey=196537e4a5814693b334cac3e4723a38"
  
     # fetching data in json format
     res = requests.get(main_url, params=query_params)
