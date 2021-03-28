86a623292d16d3c5e88cc5b218f69055dce29b84
Jacob Sandler <jsandler31415@gmail.com>
2021-03-28 02:25:24 -0700

    Revert "Merge branch 'main' of https://github.com/SShah-GH/LAHacks2021 into main"
    This reverts commit c02816d5980f663135447daf94fd0dfe5de6f2d3, reversing
    changes made to 9b57dce6b39d2ba51220cbafa209c1de40139a1c.

--- gcp/Flask_App/getAll_cloud.py
+++ gcp/Flask_App/getAll_cloud.py
@@ -1,4 +1,4 @@
-# imports
+
 import flask
 from flask import Flask, render_template
 from flask_cors import CORS
@@ -8,7 +8,7 @@ from google.cloud import language_v1
 from boilerpy3 import extractors
 import random
 import json
-
+#import helpers.py
 
 # importing requests package
 import requests
@@ -27,7 +27,7 @@ def getContent(url):
         content = extractor.get_content_from_url(url)
     else:
         content = 'error'
-    # if webscraper has failed
+    
     if content == '':
         content = 'error'
 
@@ -36,15 +36,13 @@ def getContent(url):
 def getLength(content):
 
     length = len(content)
-    # divide length by average characters read per minute
     time = length/1100.0
 
     minutes = int(round(time, 1))
-    # return string of minutes
+
     return str(minutes)
 
 def Summarize(content):
-    # post request to deep api to generate summary
     r = requests.post(
         "https://api.deepai.org/api/summarization",
         data={
@@ -71,32 +69,53 @@ def analyze_sentiment(content):
     encoding_type = language_v1.EncodingType.UTF8
 
     response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
-   
+
+    sent = dict()
+
+    # Get sentiment for all sentences in the document
+    #for sentence in response.sentences:
+    #    sent[sentence.text.content] = sentence.sentiment.score
+
+    #json_obj = json.dumps(sent)
+    #return json_obj
 
     # Get overall sentiment of the input document
     rounded_response = round((5 * response.document_sentiment.score),2)
     return str(rounded_response)
- 
+    #print(
+    #    u"Document sentiment magnitude: {}".format(
+    #       response.document_sentiment.magnitude
+    #    )
+    #)
+
 def RetreiveNews(cat):
     # BBC news api
     # following query parameters are used
     # source, sortBy and apiKey
+    call_success = False
 
-    apiKeysList = ["d7fa03ba28f94eba97ffb5276cca060a", "716c0526d1fc42008fdfd127fc0d6f9e", "a9e142b8b32a460497c02018511fbb65", "3670b6fd717a4ad0a49ba042649edef7", "196537e4a5814693b334cac3e4723a38", "39810cc78e384d3a9c416070fdeddc64"]
-    apiKey = random.choice(apiKeysList)
-    #print(apiKey)
+    while call_success == False:
+        apiKeysList = ["d7fa03ba28f94eba97ffb5276cca060a", "716c0526d1fc42008fdfd127fc0d6f9e", "a9e142b8b32a460497c02018511fbb65", "3670b6fd717a4ad0a49ba042649edef7", "196537e4a5814693b334cac3e4723a38", "39810cc78e384d3a9c416070fdeddc64"]
+        apiKey = random.choice(apiKeysList)
+        print(apiKey)
 
-    query_params = {
-      "sortBy": "top",
-      "apiKey": apiKey,
-      "category"  : cat
-    }
-    main_url = " https://newsapi.org/v2/top-headlines?country=us&category=" + cat + "&apiKey=" + apiKey
- 
-    # fetching data in json format
-    res = requests.get(main_url, params=query_params)
-    top_articles = res.json()
-    article = top_articles["articles"]
+        
+        query_params = {
+        "sortBy": "top",
+        "apiKey": apiKey,
+        "category"  : cat
+        }
+        main_url = " https://newsapi.org/v2/top-headlines?country=us&category=" + cat + "&apiKey=" + apiKey
+
+        res = requests.get(main_url, params=query_params)
+        top_articles = res.json()
+        # fetching data in json format
+        try:
+          article = top_articles["articles"]  
+        except KeyError as e:
+            call_success = False
+            continue
+        call_success = True
 
     results = []
 
@@ -106,7 +125,7 @@ def RetreiveNews(cat):
     return results
 
 
-def listFill(category_list, top_results):
+def listFill(category_list, top_results, used_articles):
 
     if len(category_list) == 0:
         rand_cat = 'general'
@@ -115,7 +134,7 @@ def listFill(category_list, top_results):
 
     while(len(top_results) < 5): #if list is not full we will pull articles from a random category    
         for ar in RetreiveNews(rand_cat):
-            if ar not in top_results:
+            if ar not in top_results and ar not in used_articles:
                 top_results.append(ar)
                 break
 
@@ -123,35 +142,28 @@ def listFill(category_list, top_results):
 
 
 def getTop5(category_list):
-    # this list will store our top 5 articles
-    top_results = [] 
+    top_results = [] #this list will store our top 5 articles
     general_article_set = []
     print_first = True
-    for ar in RetreiveNews('general'):  
-       # creating a set of general articles, ranked only by popularity, not category
-       general_article_set.append(ar) 
-    # if we have no categories selected, we just generate our list using our general set
-    if len(category_list) == 0: 
+    for ar in RetreiveNews('general'):  #TODO FIND OUT WHAT THE RESPONSE OBJECT RETURNS
+       general_article_set.append(ar) #creating a set of general articles, ranked only by popularity, not category
+    if len(category_list) == 0: # if we have no categories selected, we just generate our list using our general set
         for i in range(5):
-            # getting most popular aritcles regardless of catgories from the top of the set
-            elem = general_article_set.pop(i) 
+            elem = general_article_set.pop(i) #getting most popular aritcles regardless of catgories from the top of the set
             top_results.append(elem)  
         return top_results
-    # if list isnt empty, loop through categories
-    for category in category_list: 
+
+    for category in category_list: #if list isnt empty, loop through categories
         temp_list = []
-        # creating list of articles ranked by popularity for each category
-        templist = list(RetreiveNews(category)) 
+        templist = list(RetreiveNews(category)) #creating list of articles ranked by popularity for each category
         for i in range(20):
-            # if the first article is also in the general article_set, we add it
-            if templist[i] in general_article_set: 
+            if templist[i] in general_article_set: # if the first article is also in the general article_set, we add it
                 top_results.append(templist[i])
-                # if our list is already full, return
-                if (len(top_results) == 5): 
+                if (len(top_results) == 5): #if our list is already full, terminate function
                     return top_results
 
 
-    # Fill in extra categories if not at max articles
+    #Fill in extra categories if still missing
     used_articles = []
     top_results = listFill(category_list, top_results, used_articles)
 
@@ -192,17 +204,17 @@ def NewsHeadlines(request):
                 break
 
             for i in range(len(filtered_results)):
-                # check if article is unique
+                #check if article is unique
                 if((fuzz.ratio(ar["title"], filtered_results[i]["title"]) > 40)):  
                         isUnique = False
                         break
 
-            # If article is not unique replace it
+            #If article is not unique replace it
             if(not isUnique):
                 used_articles.append(top_results.pop(article_num))
                 top_results = listFill(cat_list, top_results, used_articles)
                 article_num = article_num + 1 
-            # If article is unique add it to the list
+            #If article is unique add it to the list
             else:
                 filtered_results.append(ar)
                 used_articles.append(ar)
@@ -215,12 +227,16 @@ def NewsHeadlines(request):
         print(ar['url'])
         content = getContent(ar['url'])
         summary = Summarize(content)
+        #sentiment = analyze_sentiment(content)
         read_time = getLength(content)
+
         ar['summary'] = summary['output']
+        #ar['sentiment'] = sentiment
         ar['read_time'] = read_time
 
     print('Appended Data')
-    # jsonify results 
+
+    #result = json.dumps(filtered_results[0])
     result = flask.jsonify({'Articles' : filtered_results})
     result.headers.add('Access-Control-Allow-Origin', '*')
 
