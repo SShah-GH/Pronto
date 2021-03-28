1e651b705917b3976a8879451648fe885990f9b8
Satva <satvabshah@gmail.com>
2021-03-28 03:29:20 -0700

    Fixed backend files

--- gcp/getNews_cloud.py
+++ gcp/getNews_cloud.py
@@ -17,30 +17,22 @@ def RetreiveNews(cat):
     # BBC news api
     # following query parameters are used
     # source, sortBy and apiKey
-    call_success = False
 
-    while call_success == False:
-        apiKeysList = ["d7fa03ba28f94eba97ffb5276cca060a", "716c0526d1fc42008fdfd127fc0d6f9e", "a9e142b8b32a460497c02018511fbb65", "3670b6fd717a4ad0a49ba042649edef7", "196537e4a5814693b334cac3e4723a38", "39810cc78e384d3a9c416070fdeddc64"]
-        apiKey = random.choice(apiKeysList)
-        print(apiKey)
+    apiKeysList = ["d7fa03ba28f94eba97ffb5276cca060a", "716c0526d1fc42008fdfd127fc0d6f9e", "a9e142b8b32a460497c02018511fbb65", "3670b6fd717a4ad0a49ba042649edef7", "196537e4a5814693b334cac3e4723a38", "39810cc78e384d3a9c416070fdeddc64"]
+    apiKey = random.choice(apiKeysList)
+    #print(apiKey)
 
-        
-        query_params = {
-        "sortBy": "top",
-        "apiKey": apiKey,
-        "category"  : cat
-        }
-        main_url = " https://newsapi.org/v2/top-headlines?country=us&category=" + cat + "&apiKey=" + apiKey
-
-        res = requests.get(main_url, params=query_params)
-        top_articles = res.json()
-        # fetching data in json format
-        try:
-          article = top_articles["articles"]  
-        except KeyError as e:
-            call_success = False
-            continue
-        call_success = True
+    query_params = {
+      "sortBy": "top",
+      "apiKey": apiKey,
+      "category"  : cat
+    }
+    main_url = " https://newsapi.org/v2/top-headlines?country=us&category=" + cat + "&apiKey=" + apiKey
+ 
+    # fetching data in json format
+    res = requests.get(main_url, params=query_params)
+    top_articles = res.json()
+    article = top_articles["articles"]
 
     results = []
 
@@ -48,6 +40,7 @@ def RetreiveNews(cat):
         results.append(ar)
 
     return results
+
 
 def listFill(category_list, top_results):
 
@@ -66,29 +59,37 @@ def listFill(category_list, top_results):
 
 
 def getTop5(category_list):
-    top_results = [] #this list will store our top 5 articles
+    # this list will store our top 5 articles
+    top_results = [] 
     general_article_set = []
     print_first = True
-    for ar in RetreiveNews('general'):  #TODO FIND OUT WHAT THE RESPONSE OBJECT RETURNS
-       general_article_set.append(ar) #creating a set of general articles, ranked only by popularity, not category
-    if len(category_list) == 0: # if we have no categories selected, we just generate our list using our general set
+    for ar in RetreiveNews('general'):  
+       # creating a set of general articles, ranked only by popularity, not category
+       general_article_set.append(ar) 
+    # if we have no categories selected, we just generate our list using our general set
+    if len(category_list) == 0: 
         for i in range(5):
-            elem = general_article_set.pop(i) #getting most popular aritcles regardless of catgories from the top of the set
+            # getting most popular aritcles regardless of catgories from the top of the set
+            elem = general_article_set.pop(i) 
             top_results.append(elem)  
         return top_results
-
-    for category in category_list: #if list isnt empty, loop through categories
+    # if list isnt empty, loop through categories
+    for category in category_list: 
         temp_list = []
-        templist = list(RetreiveNews(category)) #creating list of articles ranked by popularity for each category
+        # creating list of articles ranked by popularity for each category
+        templist = list(RetreiveNews(category)) 
         for i in range(20):
-            if templist[i] in general_article_set: # if the first article is also in the general article_set, we add it
+            # if the first article is also in the general article_set, we add it
+            if templist[i] in general_article_set: 
                 top_results.append(templist[i])
-                if (len(top_results) == 5): #if our list is already full, terminate function
+                # if our list is already full, return
+                if (len(top_results) == 5): 
                     return top_results
 
 
-    #Fill in extra categories if still missing
-    top_results = listFill(category_list, top_results)
+    # Fill in extra categories if not at max articles
+    used_articles = []
+    top_results = listFill(category_list, top_results, used_articles)
 
     return top_results
 
@@ -99,7 +100,8 @@ def NewsHeadlines(request):
 
     # Get data from url
     categories = request.args.get('categories', default = '')
-    cat_list = categories.split()
+    cat_list = categories.split('-')
+    print(cat_list)
 
     
     top_results = getTop5(cat_list)
@@ -130,41 +132,8 @@ def NewsHeadlines(request):
                     break
                 article_num = article_num + 1 
 
-    
-
     #result = json.dumps(filtered_results[0])
     result = flask.jsonify({'Articles' : filtered_results})
     result.headers.add('Access-Control-Allow-Origin', '*')
 
-
     return result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-       
-
-
-
